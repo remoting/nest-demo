96520d57ca6b5adaa01a0763c9750b2b9b497142
liyong <liyong@lanren-inc.com>
2022-12-21 16:50:39 +0800

    init

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+const env = process.env.NODE_PROFILE || 'dev';
+let logPath = './logs/';
+if (env.toLowerCase() != 'dev') {
+  logPath = '/data/logs/';
+}
+module.exports = {
+  apps: [
+    {
+      name: 'server',
+      exec_mode: 'cluster',
+      instances: 3,
+      instance_var: 'INSTANCE_ID',
+      //error_file: logPath + "err.log",//错误输出日志
+      //out_file: logPath + "out.log", //日志
+      script: './dist/main.js',
+      //log_date_format: "YYYY-MM-DD HH:mm:ss.SSS "
+    },
+  ],
+};
